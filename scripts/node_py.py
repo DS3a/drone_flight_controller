cf612293e890186b584e20f2de5f71c52005554a
DS3a <33351122+DS3a@users.noreply.github.com>
2024-01-09 23:21:11 +0530

    use custom message

--- scripts/node_py.py
+++ scripts/node_py.py
@@ -7,19 +7,20 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 import time
+from drone_controller_messages.msg import PwmMessage
 
 def main():
-    hello()
     rclpy.init()
     test_node = Node("node_py_name")
+    msg = PwmMessage()
+    msg.front_left = 20
+    msg.front_right = 2
+    print(msg)
 
-    str_publisher = test_node.create_publisher(String, "/topicdd", 10)
+    str_publisher = test_node.create_publisher(PwmMessage, "/topicdd", 10)
 
-    msg = String()
-    msg.data = "hello"
     for i in range(10):
         str_publisher.publish(msg)
-        hello()
         time.sleep(1)
 
     return True
